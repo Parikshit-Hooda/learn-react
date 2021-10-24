b6729b1f3bd9b7eb862cc5e7c9717abbca88a58a
Parikshit Hooda <phooda804@live.com>
2021-10-25 05:22:11 +0530

    learned 'extract to new method vscode func + using expressions to calculate class|

---
+++ src/components/counter.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+class Counter extends React.Component {
+  state = {
+    count:0,
+  };
+
+
+
+  render() { 
+
+    let classes = this.getBadgeClasses();
+
+    return <div>
+    <span className = {classes}>{this.fn1()}</span>
+    <button class = "btn btn-secondary btn-sm">Increment</button></div>;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
+
+  // classval() {
+  //   return 
+  // }
+
+  fn1(){
+    const { count } = this.state;
+    return count === 2 ? ":)" : ":(";
+  }
+}
+ 
+export default Counter;
